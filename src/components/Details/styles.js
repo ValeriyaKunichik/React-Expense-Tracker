5aef15d333faf995016c46922245443587f6bdcc
Valeriya <kunichikval@gmail.com>
2023-01-23 09:22:48 +0600

    Deployed

---
+++ src/components/Details/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  income: {
+    borderBottom: '10px solid green',
+  },
+  expense: {
+    borderBottom: '10px solid red',
+  },
+}));
